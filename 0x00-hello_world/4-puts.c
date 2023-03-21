9a44cbe00f6f354773d42bebc4d2f30291bd6b20
pearlly548 <morrisblessing548@gmil.com>
2023-03-21 15:02:48 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+/* This code prints out a message on programming */
+
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * * Description: 'Print out a quote about programming'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	/**
+	 *  The puts function here acts like the printf to display
+	 *  text written within the braces
+	 */
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
