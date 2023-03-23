655830acef0ecb81ae870e55205b7a9dcc20f631
pearlly548 <morrisblessing548@gmil.com>
2023-03-23 06:35:58 -0700

    0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char *pu = "_putchar";
+
+	while (*pu)
+	{
+		putchar(*pu);
+		pu++;
+	}
+	putchar('\n');
+	return (0);
+}
