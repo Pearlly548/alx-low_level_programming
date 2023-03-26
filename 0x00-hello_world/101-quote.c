380f1de21f87681639e1e5a99ba9a134b8555f85
pearlly548 <morrisblessing548@gmil.com>
2023-03-26 15:09:25 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,12 @@
-/* This code prints with an error */
 #include <stdio.h>
 #include <unistd.h>
 /**
  * main - entry point
  *
- * Description: Code as alternative to print
- *
- * Return: if return 1 (Eror)
+ * Return: if return 1
  */
 int main(void)
 {
-	/*
-	 * write - is a function that points to the screen using system call
-	 */
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (0);
 }
