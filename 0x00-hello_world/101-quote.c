7fd1f99baaf0396535446702a5a3f93f6f0188ab
pearlly548 <morrisblessing548@gmil.com>
2023-03-26 15:26:04 -0700

    101 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
  * main - entry point
- *
- * Return: if return 1
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
