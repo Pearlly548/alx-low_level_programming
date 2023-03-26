de8c916b2c3108f1fb4eccfac64fdbd30fe4aab9
pearlly548 <morrisblessing548@gmil.com>
2023-03-26 12:14:57 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
