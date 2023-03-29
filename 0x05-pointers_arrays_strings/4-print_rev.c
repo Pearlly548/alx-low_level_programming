052783de8fadd0cb0924e0c05f67e5d2cf411232
pearlly548 <morrisblessing548@gmil.com>
2023-03-28 20:02:38 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints_rev - prints a string in reverse
+ * @s: sring
+ * return: 0
+ */
+void print_rev(char *s);
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (i = i - 1; s[i] != '0'; i - -)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
