2833faf6283da8ab916886ebca2d69b6dae51642
pearlly548 <morrisblessing548@gmil.com>
2023-03-30 10:00:24 -0700

    7 - leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encoding leet
+ * @str:encoded string
+ * Return: the value of n
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
