63e9a5fb5b736309ee91cee364dc859d6f468aaf
pearlly548 <morrisblessing548@gmil.com>
2023-03-30 11:33:03 -0700

    task 100

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,8 +2,7 @@
 
 /**
  * print_number - Prints an integer.
- * @n: The integer to be printed
- * Return: 0
+ * @n: The integer to be printed.
  */
 
 void print_number(int n)
@@ -13,12 +12,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }
