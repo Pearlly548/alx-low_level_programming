14938e766cface38db3ae884c3112e9d2d60d43c
pearlly548 <morrisblessing548@gmil.com>
2023-04-03 14:29:29 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: memory area
+ * @src: source
+ * @n: length of src
+ *
+ * Return: pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+	{
+		dest[c] = src[c];
+	}
+	return (dest);
+}
+
