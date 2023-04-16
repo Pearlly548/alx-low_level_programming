a82e36673f12d41022c75eb06735a944aaaaa591
pearlly548 <morrisblessing548@gmil.com>
2023-04-16 13:29:06 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,77 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-
 /**
- * main - program minimum number of coins to make change
- * @argc: the amount of arguments passed into the program
+ * main - program prints amount of coins to make change
+ * @argc: the number of arguments passed into the program
  * @argv: an array of strings passed to the program
  *
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	int coinConverter(int index)
-{
-	int count = 0;
+	int cents, numcoins;
 
-	while (index != 0)
-	{
-		if (index % 10 == 9 || index % 10 == 7)
-			index -= 2;
-		else if (index % 25 == 0)
-			index -= 25;
-		else if (index % 10 == 0)
-			index -= 10;
-		else if (index % 5 == 0)
-			index -= 5;
-			else if (index % 2 == 0)
-		{
-			if (index % 10 == 6)
-				index -= 1;
-			else
-				index -= 2;
-		}
-		else
-			index -= 1;
-
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
- */
-int main(int argc, char *argv[])
-{
-	int index, coin;
-
-	coin = 0;
-
+	numcoins = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	index = atoi(argv[1]);
-
-	if (index < 0)
-		printf("0\n");
-
-	else
+	cents = atoi(argv[1]);
+	while (cents > 0)
 	{
-		coin = coinConverter(index);
-
-		printf("%d\n", coin);
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		numcoins += 1;
 	}
-
+	printf("%d\n", numcoins);
 	return (0);
 }
-
