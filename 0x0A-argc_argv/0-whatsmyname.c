709968ee99030e97bfd850983c965911e2363a0c
pearlly548 <morrisblessing548@gmil.com>
2023-04-16 10:49:01 -0700

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the code
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0(succes)
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%d\n", *argv[argc]);
+	return (0);
+}
