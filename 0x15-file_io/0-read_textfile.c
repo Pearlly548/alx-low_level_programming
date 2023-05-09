3147754a0ad5026bad0cf11df1c20e30e7f82f79
Pearlly548 <morrisblessing548@gmail.com>
2023-05-09 03:07:14 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -13,40 +12,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
-	ssize_t n_read, n_written;
-	char *buffer;
+	ssize_t n_In, n_Out;
+	char *fptr;
 
 	if (filename == NULL)
-		return (-1);
+		return (0);
 
 	file = open(filename, O_RDONLY);
 	if (file == -1)
-		return (-1);
-
-	buffer = malloc((letters + 1) * sizeof(char));
-	if (buffer == NULL)
 	{
-		close(file);
-		return (-1);
+		return (0);
 	}
 
-	n_read = read(file, buffer, letters);
-	if (n_read == -1)
-	{
-		free(buffer);
-		close(file);
-		return (-1);
-	}
+	fptr = malloc(letters * sizeof(char));
+	if (fptr == NULL)
+		return (0);
 
-	n_written = write(STDOUT_FILENO, buffer, n_read);
-	if (n_written == -1 || n_written != n_read)
-	{
-		free(buffer);
-		close(file);
-		return (-1);
-	}
+	n_In = read(file, fptr, letters);	
+	n_Out = write(STDOUT_FILENO, fptr, n_In);
+	
+	
+	if (n_Out != n_In || n_Out == -1)
+		return (0);
 
-		free(buffer);
-		close(file);
-	return (n_read);
+	free(fptr);
+	close(file);
+	return (n_In);
 }
