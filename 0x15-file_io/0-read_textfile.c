20b737f9b977d0b021595f433c21e375866b19ce
Pearlly548 <morrisblessing548@gmail.com>
2023-05-09 03:12:39 -0700

    0 task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	n_Out = write(STDOUT_FILENO, fptr, n_In);
 	
 	
-	if (n_Out != n_In || n_Out == -1)
+	if (n_Out != n_In && n_Out == -1)
 		return (0);
 
 	free(fptr);
